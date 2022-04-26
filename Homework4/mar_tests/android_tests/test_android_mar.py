9086c7a719e972889fd13800e959e6a72397087d
DmtrSuch <15111976@inbox.ru>
2022-04-26 21:55:04 +0300

    resolve

--- Homework4/mar_tests/android_tests/test_android_mar.py
+++ Homework4/mar_tests/android_tests/test_android_mar.py
@@ -4,10 +4,12 @@ import pytest
 import random
 from mar_tests.base import BaseCase
 
-
+population = '146'
+timer = '00:'
+mail = 'Mail.ru'
 
 class TestAndroid(BaseCase):
-
+    @pytest.mark.skip
     @pytest.mark.AndroidUI
     def test_population(self):
         self.main_page.skip_notification()
@@ -15,14 +17,16 @@ class TestAndroid(BaseCase):
         self.main_page.search('Russia')
         self.main_page.swipe_element_lo_left(self.main_page.locators.SWIPE_POPULATION)
         self.main_page.click_for_android(self.main_page.locators.SWIPE_CLICK)
-        assert self.main_page.find(self.main_page.locators.BIG_ANSWER_LOCATOR).text in self.driver.page_source
+        assert population in self.main_page.find(self.main_page.locators.BIG_ANSWER_LOCATOR).text
 
+    @pytest.mark.skip
     @pytest.mark.AndroidUI
     def test_calc(self):
         self.main_page.skip_notification()
         self.main_page.use_keyboard()
         self.main_page.search(f'{random.randint(0,999)}+{random.randint(0,999)}')
 
+    @pytest.mark.skip
     @pytest.mark.AndroidUI
     def test_news(self):
         self.main_page.skip_notification()
@@ -31,10 +35,7 @@ class TestAndroid(BaseCase):
         self.menu_page.exit()
         self.main_page.use_keyboard()
         self.main_page.search('News')
-        assert self.main_page.find(self.main_page.locators.TURN_NEWS1_LOCATION).text \
-        or self.main_page.find(self.main_page.locators.TURN_NEWS2_LOCATION).text \
-        or self.main_page.find(self.main_page.locators.PLAY_NEWS_LOCATION).text \
-        in self.driver.page_source
+        assert timer in self.main_page.find(self.main_page.locators.TURN_NEWS1_LOCATION).text
 
 
     @pytest.mark.AndroidUI
@@ -44,5 +45,5 @@ class TestAndroid(BaseCase):
         self.main_page.skip_notification()
         self.main_page.go_to_menu()
         self.menu_page.checkversion()
-        assert self.menu_page.find(self.menu_page.locators.COPYRIGHT_LOCATOR).text in self.driver.page_source
+        assert mail in self.menu_page.find(self.menu_page.locators.COPYRIGHT_LOCATOR).text
         assert FILEVERSION in self.menu_page.find(self.menu_page.locators.VERSION_APP_LOCATOR).text
